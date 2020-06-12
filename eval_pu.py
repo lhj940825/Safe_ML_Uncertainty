fixed wrong dimension issue in NLL loss function and Target normalization issue

eval_pu.py
@@ -26,14 +26,14 @@ if __name__ == '__main__':
     output_dirs = {}
     output_dirs["boston"] = []
     output_dirs["wine"] =  []
-    # output_dirs["power_plant"] =  []
-    # output_dirs["concrete"] =  []
-    # output_dirs["energy"] = []
-    # output_dirs["kin8nm"] = []
-    # output_dirs["naval"] =  []
-    # output_dirs["yacht"] =  []
-    # output_dirs["protein"] =  []
-    # output_dirs["year"] =  []
+    output_dirs["power_plant"] =  []
+    output_dirs["concrete"] =  []
+    output_dirs["energy"] = []
+    output_dirs["kin8nm"] = []
+    #output_dirs["naval"] =  []
+    output_dirs["yacht"] =  []
+    #output_dirs["protein"] =  []
+    #output_dirs["year"] =  []
 
     for key, output_dir in output_dirs.items():
         output_dirs[key] = os.path.join('./output', key, 'parametric_uncertainty')
@@ -57,10 +57,10 @@ if __name__ == '__main__':
     test_loaders = {}
 
     for key, fname in data_files.items():
-        train_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[0]), testing=False)
+        train_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[0]), testing=True)
         train_loaders[key] = torch.utils.data.DataLoader(train_datasets[key], batch_size=cfg["batch_size"], num_workers=0, collate_fn = train_datasets[key].collate_batch)
 
-        test_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[2]), testing=False)
+        test_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[2]), testing=True)
         test_loaders[key] = torch.utils.data.DataLoader(test_datasets[key],
                                                    batch_size=cfg["batch_size"],
                                                    num_workers=0,

model/pu_fc.py
@@ -5,7 +5,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 
 class pu_fc(nn.Module):
 
@@ -46,11 +46,17 @@ class custom_NLL():
     def __init__(self):
         pass
 
-    def __call__(self, label, mean: torch.autograd.Variable, std: torch.autograd.Variable):
-        std = torch.exp(std)
+    def __call__(self, label, mean: torch.autograd.Variable, output2: torch.autograd.Variable):
+        label = label.view(-1)
+        std = torch.exp(output2)
+        #print('std',np.shape(std))
         var = torch.pow(std,2)
+        #print('var',np.shape(var))
+        #print('label',np.shape(label))
+        #print('mean',np.shape(mean))
         NLL = torch.log(var)*0.5 + torch.div(torch.pow((label-mean),2), 2*var)
         NLL = torch.clamp(NLL, -100) # cap NLL values at -100
+        #print(np.shape(NLL))
         NLL = torch.sum(NLL)
 
         return NLL

train_pu.py
@@ -31,14 +31,14 @@ if __name__ == "__main__":
     output_dirs = {}
     output_dirs["boston"] = []
     output_dirs["wine"] =  []
-    # output_dirs["power_plant"] =  []
-    # output_dirs["concrete"] =  []
-    # output_dirs["energy"] = []
-    # output_dirs["kin8nm"] = []
-    # output_dirs["naval"] =  []
-    # output_dirs["yacht"] =  []
-    # output_dirs["protein"] =  []
-    #output_dirs["year"] =  []
+    output_dirs["power_plant"] =  []
+    output_dirs["concrete"] =  []
+    output_dirs["energy"] = []
+    output_dirs["kin8nm"] = []
+    output_dirs["naval"] =  []
+    output_dirs["yacht"] =  []
+    output_dirs["protein"] =  []
+    output_dirs["year"] =  []
 
     for key, output_dir in output_dirs.items():
         output_dirs[key] = os.path.join('./output', key, 'parametric_uncertainty')
@@ -63,13 +63,13 @@ if __name__ == "__main__":
 
     print("Prepare training data")
     for key, fname in data_files.items():
-        train_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[0]),testing=False)
+        train_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[0]),testing=True)
         train_loaders[key] = torch.utils.data.DataLoader(train_datasets[key],
                                                          batch_size=cfg["batch_size"],
                                                          num_workers=0,
                                                          collate_fn=train_datasets[key].collate_batch)
 
-        eval_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[1]), testing=False)
+        eval_datasets[key] = UCIDataset(os.path.join(data_dirs[key], fname[1]), testing=True)
         eval_loaders[key] = torch.utils.data.DataLoader(eval_datasets[key],
                                                         batch_size=cfg["batch_size"],
                                                         num_workers=0,

utils/eval_utils.py
@@ -212,8 +212,8 @@ def pu_eval(model, test_loader, cfg, output_dir, tb_logger=None, title=""):
     #gt_M_distance_list = []
     #sample_M_distance_list = []
 
-    dataset_name = output_dir.split("/")[2]
-    # dataset_name = output_dir.split("\\")[1]
+    #dataset_name = output_dir.split("/")[2]
+    dataset_name = output_dir.split("\\")[1]
 
     with torch.no_grad():
 
@@ -513,8 +513,8 @@ def pu_eval_with_training_dataset(model, train_loader, cfg, output_dir, tb_logge
     #gt_M_distance_list = []
     #sample_M_distance_list = []
 
-    dataset_name = output_dir.split("/")[2]
-    # dataset_name = output_dir.split("\\")[1]
+    #dataset_name = output_dir.split("/")[2]
+    dataset_name = output_dir.split("\\")[1]
     with torch.no_grad():
 
         for cur_it, batch in enumerate(train_loader):